37a24f825974fb22ba5d5ac59dec9311ab7c2864
Admin <Admin@ADMIN>
2022-11-11 05:29:08 +0700

    - Download code from thay Tuan

---
+++ MDK-ARM/RTE/_HC_SR04/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HC_SR04' 
+ * Target:  'HC_SR04' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
